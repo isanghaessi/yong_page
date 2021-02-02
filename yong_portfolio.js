79a8fea8a5323f0aa02bb78fac8eb3f64e617ba3
isanghaessi <jesse1231@naver.com>
2021-02-02 21:40:29 +0900

    adding dark mode

--- yong_portfolio.js
+++ yong_portfolio.js
@@ -1,3 +1,28 @@
+$(document).ready(function(){
+    var check = $("input[type='checkbox']");
+check.click(function(){
+    $("#day, #night").toggle();
+    ToggleDark();
+});
+})
+function ToggleDark() {
+    if($('#night').css('display')!='none'){
+        $('header').css('background','white');
+        $('h1, #day, #night').css('color','black')
+        $('.nav_btn').css('color','white');
+        $('.nav_btn').css('background','black');
+        $('.nav_item').css('background','black');
+        $('.nav_item').css('color','white');
+    }else{
+        $('header').css('background','black');
+        $('h1, #day, #night').css('color','white')
+        $('.nav_btn').css('color','black');
+        $('.nav_btn').css('background','white');
+        $('.nav_item').css('background','white');
+        $('.nav_item').css('color','black');
+    }
+}
+
 function MoveTo(tag){
     var offset=$('#'+tag).offset();
         $('html, body').animate({scrollTop : offset.top-100}, 500);
