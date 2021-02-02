7d1aefe01ec87bec207cb38b3adca3d1d25c0a59
isanghaessi <jesse1231@naver.com>
2021-02-02 22:20:07 +0900

    fixing nav_btn:hover

--- yong_portfolio.js
+++ yong_portfolio.js
@@ -1,23 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var check = $("input[type='checkbox']");
-check.click(function(){
-    $("#day, #night").toggle();
-    ToggleDark();
+    check.click(function () {
+        $("#day, #night").toggle();
+        ToggleDark();
+    });
+
+    $('.nav_btn').hover(function(){
+        $(this).css('background','gray');
+    },function(){
+        if($('#night').css('display')!='none'){
+            $(this).css('background','black');
+        }else{
+            $(this).css('background','white');
+        }
+    });
 });
-})
+
 function ToggleDark() {
     if($('#night').css('display')!='none'){
         $('header').css('background','white');
-        $('h1, #day, #night').css('color','black')
+        $('h1, #day, #night').css('color','black');
         $('.nav_btn').css('color','white');
         $('.nav_btn').css('background','black');
         $('.nav_item').css('background','black');
         $('.nav_item').css('color','white');
     }else{
         $('header').css('background','black');
-        $('h1, #day, #night').css('color','white')
+        $('h1, #day, #night').css('color','white');
         $('.nav_btn').css('color','black');
         $('.nav_btn').css('background','white');
+        $('.nav_btn:hover').css('background','gray');
         $('.nav_item').css('background','white');
         $('.nav_item').css('color','black');
     }
