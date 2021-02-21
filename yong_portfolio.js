4b25adbfc5dc47da181630b297759ddd3c2e189d
isanghaessi <jesse1231@naver.com>
2021-02-21 18:34:16 +0900

    removing day/night / rename value_name

--- yong_portfolio.js
+++ yong_portfolio.js
@@ -1,40 +1,17 @@
 $(document).ready(function () {
-    var check = $("input[type='checkbox']");
-    check.click(function () {
-        $("#day, #night").toggle();
-        ToggleDark();
-    });
+	var check = $("input[type='checkbox']");
 
-    $('.nav_btn').hover(function(){
-        $(this).css('background','gray');
-    },function(){
-        if($('#night').css('display')!='none'){
-            $(this).css('background','black');
-        }else{
-            $(this).css('background','white');
-        }
-    });
+	$('.nav_btn').hover(
+		function () {
+			$(this).css('background', 'gray');
+		},
+		function () {
+			$(this).css('background', 'white');
+		},
+	);
 });
 
-function ToggleDark() {
-    if($('#night').css('display')!='none'){
-        $('header').css('background','white');
-        $('h1, #day, #night').css('color','black');
-        $('.nav_btn').css('color','white');
-        $('.nav_btn').css('background','black');
-        $('.nav_item').css('background','black');
-        $('.nav_item').css('color','white');
-    }else{
-        $('header').css('background','black');
-        $('h1, #day, #night').css('color','white');
-        $('.nav_btn').css('color','black');
-        $('.nav_btn').css('background','white');
-        $('.nav_item').css('background','white');
-        $('.nav_item').css('color','black');
-    }
-}
-
-function MoveTo(tag){
-    var offset=$('#'+tag).offset();
-        $('html, body').animate({scrollTop : offset.top-100}, 250);
+function MoveTo(tag) {
+	var offset = $('#' + tag).offset();
+	$('html, body').animate({ scrollTop: offset.top - 100 }, 250);
 }
