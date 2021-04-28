81225a85da41953988719dc87481ffbc448ca8e6
isanghaessi <jesse1231@naver.com>
2021-04-28 21:00:52 +0900

    remove jquery to vanilla js

--- script.js
+++ script.js
@@ -1,46 +1,58 @@
 const HEADER_PIXEL = 100;
 const AUTO_SCROLL_TIME = 500;
 
-const menus_string = ['Intro', 'Skills', 'Projects'];
-const menus_offset = menus_string.map((v) => {
-	return $(`#${v + '_item'}`).offset();
+const menus = ['Intro', 'Skills', 'Projects'].map((m) => {
+	return {
+		menu: m,
+		top: document.querySelector(`#${m}_item`).getBoundingClientRect().top
+	};
 });
+menus.push({
+	menu: 'Github',
+	top: Infinity
+});
+console.log(menus);
 
-$(document).ready(() => {
-	$('.nav_btn').hover(
-		function () {
-			$(this).css('background', 'gray');
-		},
-		function () {
+document.addEventListener('DOMContentLoaded', () => {
+	const btns = document.querySelectorAll('.nav_btn');
+	for (let b of btns) {
+		b.addEventListener('mouseover', () => {
+			b.style.background = 'gray';
+		});
+		b.addEventListener('mouseout', () => {
 			changeCurrentBtnGray();
-		}
-	);
+		});
+	}
 
-	$(window).on('scroll', changeCurrentBtnGray);
+	window.addEventListener('scroll', changeCurrentBtnGray);
 });
 
 function changeCurrentBtnGray() {
-	const scroll_top = $(this).scrollTop();
+	const scroll_top = window.scrollY;
 	let idx_cur_item = 0;
-	for (let i of menus_offset) {
+	for (let i of menus) {
 		if (i.top - HEADER_PIXEL <= scroll_top) {
-			idx_cur_item = menus_offset.indexOf(i);
+			idx_cur_item = menus.indexOf(i);
 		}
 	}
-	for (let i = 0; i < menus_string.length; i++) {
+	for (let i = 0; i < menus.length; i++) {
 		if (i === idx_cur_item) {
-			$(`#${menus_string[i] + '_btn'}`).css('background', 'gray');
+			document.querySelector(`#${menus[i].menu}_btn`).style.background =
+				'gray';
 		} else {
-			$(`#${menus_string[i] + '_btn'}`).css('background', 'white');
+			document.querySelector(`#${menus[i].menu}_btn`).style.background =
+				'white';
 		}
 	}
 }
 
 function moveTo(from) {
 	const dest = from.replace('btn', 'item');
-	const offset = $('#' + dest).offset();
-	$('html, body').animate(
-		{ scrollTop: offset.top - HEADER_PIXEL },
-		AUTO_SCROLL_TIME
-	);
+	const offset = menus.filter((m) => {
+		return m.menu === from.replace('_btn', '');
+	})[0];
+	window.scrollTo({
+		top: offset.top,
+		behavior: 'smooth'
+	});
 }
